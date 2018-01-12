33ed76aafec3e6118233b21e64f4ca5666cb589c
rand89ck <rand.ck@gmail.com>
2018-01-11 20:58:00 -0500

    First commit

---
+++ client/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+import {FormsModule} from '@angular/forms';
+import {AppComponent} from './app.component';
+import {TaskComponent} from './components/tasks/task.component';
+
+@NgModule({
+  imports:      [ BrowserModule, HttpModule, FormsModule ],
+  declarations: [ AppComponent, TaskComponent ],
+  bootstrap:    [ AppComponent ]
+  })
+
+export class AppModule { }
